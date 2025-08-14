580744798b3dda302d411af20181b66b054ba4d2
Lorenaatavares <157626068+Lorenaatavares@users.noreply.github.com>
2025-08-13 21:43:09 -0300

    feat/mobile-44/adicionar-splash-screen

--- app/src/main/java/com/example/sinaramobile/SplashScreen.java
+++ app/src/main/java/com/example/sinaramobile/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.example.sinaramobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,13 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(this::abrirTelaInicial, 3500);
+
+    }
+    private void abrirTelaInicial() {
+        Intent rota = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(rota);
+        finish();
     }
 }
